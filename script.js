b0b75b7994a77f6ac8228c4bae631a58782b3323
kanatmakeev <mr.kanatm@gmail.com>
2022-06-05 22:00:11 +0300

    added new task

---
+++ script.js
@@ -0,0 +1,20 @@
+// homework_03-01
+// task_1 .Даны переменные a = 10 и b = 3. Найдите остаток от деления a на b.
+const a = 10;
+const b = 3;
+const rezult = (a % b);
+
+if (rezult != 0) {
+	alert('Делится с остатком ' + rezult);
+} else {
+	alert('Делится, результат деления: ' + (a / b));
+}
+
+// Text2 2.Возведите 2 в 10 степень. Результат запишите в переменную st.
+const a1 = 2;
+const b1 = 10;
+const st = (a1 ** b1);
+
+if (true) {
+	alert('rezultat ' + st);
+} 
